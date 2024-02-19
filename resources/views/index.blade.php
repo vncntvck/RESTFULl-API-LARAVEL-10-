de77aefc75375f593e91ac35685a53f077ac0423
theking <vincentiusvicko4@gmail.com>
2024-02-19 09:01:11 +0700

    html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="checkbox" name="" id="">
+        <input type="data" name="" id="">
+        <input type="email" name="" id="">
+    </form>
+</body>
+</html>
